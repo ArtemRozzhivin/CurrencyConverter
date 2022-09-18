23e8ccca79b0e61b67fb69d2394c4df321f4d301
ArtemRozzhivin <temchik200352@gmail.com>
2022-09-18 16:03:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Block } from './Block';
 import './index.scss';
+import arrows from './assets/arrows.svg';
 
 function App() {
   const ratesRef = useRef({});
-  const [fromCurrency, setFromCurrency] = useState('UAH');
-  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('UAH');
   const [fromValue, setFromValue] = useState(0);
   const [toValue, setToValue] = useState(0);
+  //   const [changeBlock, setChangeBlock] = useState(false);
 
   useEffect(() => {
     fetch('https://cdn.cur.su/api/latest.json')
       .then((res) => res.json())
       .then((json) => {
         ratesRef.current = json.rates;
-        changeToCurRates(1);
+        changeFromCurRates(1);
       });
   }, []);
+
+  const handleChangeBlock = () => {
+    const fromCur = fromCurrency;
+    const fromVal = fromValue;
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCur);
+    setFromValue(toValue);
+    setToValue(fromVal);
+  };
 
   const changeFromCurRates = (value) => {
     const price = value / ratesRef.current[fromCurrency];
@@ -26,10 +37,8 @@ function App() {
   };
 
   const changeToCurRates = (value) => {
-    const price = value / ratesRef.current[toCurrency];
-    const result = price * ratesRef.current[fromCurrency];
-    setFromValue(result.toFixed(3));
-    setToValue(value);
+    const result = (ratesRef.current[toCurrency] / ratesRef.current[fromCurrency]) * value;
+    setToValue(result.toFixed(3));
   };
 
   useEffect(() => {
@@ -37,23 +46,28 @@ function App() {
   }, [fromCurrency]);
 
   useEffect(() => {
-    changeToCurRates(toValue);
+    changeToCurRates(fromValue);
   }, [toCurrency]);
 
   return (
     <div className="App">
-      <Block
-        value={fromValue}
-        onChangeValue={changeFromCurRates}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrency}
-      />
-      <Block
-        value={toValue}
-        onChangeValue={changeToCurRates}
-        currency={toCurrency}
-        onChangeCurrency={setToCurrency}
-      />
+      <div className="content">
+        <Block
+          value={fromValue}
+          onChangeValue={changeFromCurRates}
+          currency={fromCurrency}
+          onChangeCurrency={setFromCurrency}
+        />
+        <button className="btn" onClick={() => handleChangeBlock()}>
+          <img src={arrows} alt=""></img>
+        </button>
+        <Block
+          value={toValue}
+          onChangeValue={changeToCurRates}
+          currency={toCurrency}
+          onChangeCurrency={setToCurrency}
+        />
+      </div>
     </div>
   );
 }
